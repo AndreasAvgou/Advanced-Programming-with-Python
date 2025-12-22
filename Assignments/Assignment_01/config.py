16f7241dac1cf2c6148a1f7219cce69c017bc06f
Andreas Avgoustis <andreasav@alfaedu.gr>
2025-12-22 10:58:27 +0200

    Update config.py

--- Assignments/Assignment_01/config.py
+++ Assignments/Assignment_01/config.py
@@ -2,11 +2,12 @@
 import os
 
 # Configuration Settings
-DATA_FILENAME = 'ex1_twitter_dataset.csv'
+DATA_FILENAME = 'twitter_dataset.csv'
 RESULTS_DIR = 'results'
 PLOTS_DIR = os.path.join(RESULTS_DIR, 'plots')
 
 # Ensure output directories exist
 def setup_directories():
     os.makedirs(RESULTS_DIR, exist_ok=True)
+
     os.makedirs(PLOTS_DIR, exist_ok=True)
